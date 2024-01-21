040582675163758f042eeef8fc4af205ec740cf7
Thiago Sawada <tms1_tms2@hotmail.com>
2024-01-21 16:25:34 -0300

    feat: sign in

--- javascript/application.js
+++ javascript/application.js
@@ -1,1 +1,18 @@
-console.log("Hello from JavaScript!");
+const url = "https://reqres.in/api/register"
+
+const signUp = (event) => {
+  event.preventDefault()
+
+  const requestDetails = {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(Object.fromEntries(new FormData(form)))
+  }
+
+  fetch(url, requestDetails)
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+
+const form = document.querySelector("#form")
+form.addEventListener("submit", signUp)
